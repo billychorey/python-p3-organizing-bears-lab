ec7f32549e98566c990ef724ff3a7670e2a3aa17
Billy <billychorey@gmail.com>
2024-04-26 19:37:10 -0400

    organize bears lab

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,64 @@
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
-"""
-
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM
+        bears
+    WHERE
+        sex = 'F';
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM
+        bears
+    WHERE
+        alive = 1
+    ORDER BY
+        age ASC;
 """
 
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+# Ensure to write your SQL query for the following variable:
+select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest_with_comments = """
+    SELECT
+        name,
+        age
+    FROM
+        bears
+    WHERE
+        alive = 1
+    ORDER BY
+        age ASC;
 """
+
+
+select_oldest_bear_and_returns_name_and_age = """
+    SELECT
+        name,
+        age
+    FROM bears
+    ORDER BY age DESC 
+    LIMIT 1;
+"""
+
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM
+        bears
+    WHERE
+        age = (SELECT MIN(age) FROM bears WHERE alive = 1)
+        AND alive = 1;
+"""
+
+select_all_bears_names_and_orders_in_alphabetical_order = """
+    SELECT
+        name
+    FROM
+        bears
+    ORDER BY
+        name ASC;
 """
